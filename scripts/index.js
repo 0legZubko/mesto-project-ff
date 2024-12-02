5fd81ba3b58135519941745c6615e74a7473de0c
Oleg Zubko <pandasick@yandex.rugit config --global user.email pandasick@yandex.ru>
2024-12-03 02:05:06 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector(".content");
+const cardsContainer = container.querySelector(".places__list");
+
+function renderCards() {
+  initialCards.forEach(({ name, link }) => {
+    const card = createCard({ name, link });
+    cardsContainer.append(card);
+  });
+}
+
+function createCard({ name, link }) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__image").alt = name;
+  cardElement.querySelector(".card__title").textContent = name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => cardElement.remove());
+
+  return cardElement;
+}
+
+renderCards();
